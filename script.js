7e3a25b56a744a8ed43f1bff5b680874311de27d
nani9347 <rajinikanthgattu9347@gmail.com>
2025-02-09 01:40:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// This function will show an alert when the button is clicked
+document.getElementById("alertButton").addEventListener("click", function() {
+    alert("Hello! Thanks for clicking the button.");
+});
